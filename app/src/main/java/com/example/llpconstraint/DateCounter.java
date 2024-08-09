c2a02c1bccf6dc468d3acc30f51200f2c619a159
Phyo Hein ( Ezekiel ) <154808657+phyo-105438@users.noreply.github.com>
2024-08-09 16:39:52 +0700

    Initial Commit

--- app/src/main/java/com/example/llpconstraint/DateCounter.java
+++ app/src/main/java/com/example/llpconstraint/DateCounter.java
@@ -27,6 +27,8 @@ public class DateCounter extends AppCompatActivity {
     private ActivityDateCounterBinding binding;
     private DatePickerDialog datePickerDialog;
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    private  AlertDialog alertDialog;
+    private String name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +37,7 @@ public class DateCounter extends AppCompatActivity {
         binding = ActivityDateCounterBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         initUI();
+        initDialog();
         initListener();
     }
 
@@ -48,6 +51,35 @@ public class DateCounter extends AppCompatActivity {
             updateDate();
         });
         updateDate();
+    }
+
+    private void initDialog(){
+        var dialogBinding = DialogInputBinding.inflate(getLayoutInflater());
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        alertDialog = builder
+                .setView(dialogBinding.getRoot())
+                //This is Title and message dialog
+//                .setTitle("This is a dialog")
+//                .setMessage("I am a custom dialog for date counter")
+
+                //This is to another view
+                .setCancelable(false)
+
+                //Commenting toast message of dialog
+//                .setPositiveButton("OK", (dialog1, which) -> Toast.makeText(this, "OK", Toast.LENGTH_SHORT).show())
+//                .setNegativeButton("Cancel",((dialog1, which) -> {Toast.makeText(this, "Cancel", Toast.LENGTH_SHORT).show();}))
+                .create();
+
+        dialogBinding.btnOK.setOnClickListener(v -> {
+            // Toast.makeText(this, dialogBinding.etInput.getText().toString(), Toast.LENGTH_LONG).show();
+            binding.tvJerry.setText(dialogBinding.etInput.getText().toString());
+            alertDialog.cancel();
+        });
+
+        dialogBinding.btnCancel.setOnClickListener(v -> {
+            alertDialog.cancel();
+        });
+
     }
 
     private void updateDate(){
@@ -66,37 +98,23 @@ public class DateCounter extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                showCustomDialog();
+                showCustomDialog(name);
             }
         }
 
 
-        binding.tvTom.setOnClickListener( new OnTextClickListener());
-        binding.tvJerry.setOnClickListener(new OnTextClickListener());
+        binding.tvTom.setOnClickListener( v ->  showCustomDialog("Tom"));
+        binding.tvJerry.setOnClickListener(v ->  showCustomDialog("Jerry"));
     }
 
-    private void showCustomDialog() {
-        var dialogBinding = DialogInputBinding.inflate(getLayoutInflater());
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        AlertDialog dialog = builder
-                .setView(binding.getRoot())
-                //This is Title and message dialog
-//                .setTitle("This is a dialog")
-//                .setMessage("I am a custom dialog for date counter")
+    private void showCustomDialog(String name) {
 
-                //This is to another view
-                .setCancelable(false)
+        switch(name){
+            "Tom" -> {}
+            "Jerry" ->
+        }
 
-                //Commenting toast message of dialog
-//                .setPositiveButton("OK", (dialog1, which) -> Toast.makeText(this, "OK", Toast.LENGTH_SHORT).show())
-//                .setNegativeButton("Cancel",((dialog1, which) -> {Toast.makeText(this, "Cancel", Toast.LENGTH_SHORT).show();}))
-                .create();
-
-        dialogBinding.btnOK.setOnClickListener(v -> {
-            Toast.makeText(this, dialogBinding.etInput.getText().toString(), Toast.LENGTH_LONG).show();
-        });
-
-        dialog.show();
+        alertDialog.show();
     }
 
 }
