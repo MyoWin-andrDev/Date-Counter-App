2ccc08d47d93317a4757d076b7b5a76eaf3e6902
Phyo Hein ( Ezekiel ) <154808657+phyo-105438@users.noreply.github.com>
2024-09-14 18:34:54 +0700

    Date Counter Commit

---
+++ app/src/main/java/com/example/llpconstraint/note/AppDatabase.java
@@ -0,0 +1,23 @@
+package com.example.llpconstraint.note;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.example.llpconstraint.note.dao.NoteDao;
+import com.example.llpconstraint.note.entities.Note;
+
+@Database(entities = {Note.class} , version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+
+    public NoteDao noteDao;
+    private static AppDatabase appDatabase;
+    public synchronized static AppDatabase getInstance(Context context){
+        if(appDatabase == null){
+            appDatabase = Room.databaseBuilder(context, AppDatabase.class,"my_note_application").allowMainThreadQueries().build();
+        }
+        return appDatabase;
+    }
+}
--- app/src/main/java/com/example/llpconstraint/note/NoteMainActivity.java
+++ app/src/main/java/com/example/llpconstraint/note/NoteMainActivity.java
@@ -9,14 +9,16 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.llpconstraint.R;
+import com.example.llpconstraint.databinding.ActivityNoteMainBinding;
 
 public class NoteMainActivity extends AppCompatActivity {
-
+    private ActivityNoteMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_note_main);
+        binding = ActivityNoteMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
     }
 }
