93e27a2ceeb981b64138816a83226c380d113f62
Phyo Hein ( Ezekiel ) <154808657+phyo-105438@users.noreply.github.com>
2024-09-14 19:17:15 +0700

    Date Counter Commit

--- app/src/main/java/com/example/llpconstraint/note/EditNoteActivity.java
+++ app/src/main/java/com/example/llpconstraint/note/EditNoteActivity.java
@@ -9,18 +9,16 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.llpconstraint.R;
+import com.example.llpconstraint.databinding.ActivityEditNoteBinding;
 
 public class EditNoteActivity extends AppCompatActivity {
-
+    private ActivityEditNoteBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_edit_note);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        binding = ActivityEditNoteBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
     }
 }
