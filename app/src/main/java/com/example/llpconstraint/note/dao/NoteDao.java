72717de65b9a7fdad84531a4bff165b91b151db2
Phyo Hein ( Ezekiel ) <154808657+phyo-105438@users.noreply.github.com>
2024-09-14 17:54:32 +0700

    Date Counter Commit

--- app/src/main/java/com/example/llpconstraint/note/dao/NoteDao.java
+++ app/src/main/java/com/example/llpconstraint/note/dao/NoteDao.java
@@ -1,4 +1,18 @@
 package com.example.llpconstraint.note.dao;
 
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Update;
+
+import com.example.llpconstraint.note.entities.Note;
+
+@Dao
 public interface NoteDao {
+    @Insert
+    void addNote(Note note);
+    @Update
+    void updateNote(Note note);
+    @Delete
+    void deleteNote(Note note);
 }
